5f4f4d16d05e8f270a3eba63b95c4f4039650845
JannatulTabbassumHira <70144902+JannatulTabbassumHira@users.noreply.github.com>
2022-03-13 08:59:46 -0700

    Add files via upload

---
+++ task04/task/PublicHome.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fooide </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="full-page">
+                <header class="navigation-bar">
+                <div class="logo">
+                    <h1>Fooide</span></h1>
+                </div>>
+                <nav>
+                    <ul id='MenuItems'>
+                        <li><a href='PublicHome.php'>Home | </a></li>
+                        <li><a href='LoginPage.php'>Login | </a></li>
+                        <li><a href='RegistrationPage.php'>Registration | </a></li>
+                    </ul>
+                </nav>
+                </header> 
+                <div class="col-1">
+                    <p class='defination'>Welcome Foodie</p>
+                </div>
+            </div>
+            <footer>
+    	<p>Copyright @ 2017</p>
+    </footer>
+</body>
+</html>
